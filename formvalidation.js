7e7d7c2f58e133ee857f614ff491c0f11579597e
BlanchyP <blanchypolangcos@gmail.com>
2018-05-10 18:11:34 -0700

    upload malware reads 20 bytes

---
+++ formvalidation.js
@@ -0,0 +1,19 @@
+function isEmail(em) {
+    if (em == "") return "Please enter a valid email.\n"
+}
+
+function isAlphanumeric(un) {
+    if (un == "") return "Usernames are alphanumeric.\n"
+}
+
+function isPassword(pw) {
+    if (pw == "") return "Please enter a password.\n"
+}
+
+function valSignup(form) {
+    
+}
+
+function valSignin(form) {
+
+}
